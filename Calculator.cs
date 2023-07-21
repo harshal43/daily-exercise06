025ab21b939bc5e7a069187cc7762e0bb5d2b75d
harshal43 <harshal88843bh@gmail.com>
2023-07-21 16:25:09 +0530

    intial commit

---
+++ Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppDailyExercise02
+{
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        static int i = 2;
+        public bool isPrime(int x)
+        {
+            if (x == 0 || x == 1) return false;
+            if (x == i) return true;
+            if (x % i == 0) return false;
+            i++;
+            return isPrime(x);
+        }
+    }
+}
---
+++ CalculatorTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppDailyExercise02
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        Calculator calc = new Calculator();
+        
+        [Test]
+        public void countTestOnAdd()
+        {
+            Assert.AreEqual(10, calc.Add(5, 5));
+        }
+
+        [Test]
+        public void countTestOnSub()
+        {
+            Assert.AreEqual(20, calc.Subtract(30, 10));
+        }
+
+        [Test]
+        public void countTestOnMulti()
+        {
+            Assert.AreEqual(20, calc.Multiply(30, 10));
+        }
+
+        [Test]
+        public void countTestOnDivide()
+        {
+            Assert.AreEqual(2, calc.Divide(10, 5));
+        }
+
+        [Test]
+        public void countTestOnPrime()
+        {
+            Assert.AreEqual(3, calc.isPrime(3));
+        }
+    }
+}
